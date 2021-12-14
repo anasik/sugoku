0d205228286c6cae3741cd3746b98f1ef8a03070
Anas Ismail Khan <ankhan@Anass-MacBook-Pro.local>
2021-12-14 14:45:23 +0500

    preloader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import { getBoard, solve, grade, validate } from './js/helpers'
 import { useEffect, useState } from 'react';
-import { logo, base, ssj, ssb2, ssg } from './images'
+import { logo, base, ssj, ssb2, ssg, charging } from './images'
 
 function App() {
-
+  const [loading, setLoading] = useState(true);
   const [board, setBoard] = useState(Array(9).fill(null).map((u, i) => Array(9).fill(null)))
   const [gradeResult, setGrade] = useState('medium')
   const [validateResult, setValidate] = useState('unsolved')
@@ -30,50 +30,51 @@ function App() {
     setValidate('unsolved')
   }
 
-  useEffect(() => getBoard("medium", setBoard), [])
+  useEffect(() => getBoard("medium", (x) => { setBoard(x); setLoading(false) }), [])
 
   return (
-    <div className="App">
-      <div className='goku'>
-        <img src={source()} width={500} />
-      </div>
-      <div className='main'>
-        <h1>SuG<span><img src={logo} height={50} style={{
-          position: 'relative',
-          top: 14
-        }} /></span>KU</h1>
-        <div className='row'>
-          <div className="board">
-            {board?.map((d, i) =>
-              d?.map((e, j) =>
-                <input
-                  className={"board-item " + ((i === 0) ? "border-top " : "") + ((j === 0) ? "border-left " : "") + (((i + 1) % 3 === 0) ? "border-bottom-thick" : "")}
-                  value={e}
-                  onChange={({ target }) => {
-                    let copyBoard = board.map(d => d);
-                    copyBoard[i][j] = parseInt(target.value)
-                    setBoard(copyBoard)
-                  }}
-                />)
-            )}
+    loading ? <img src={charging} /> :
+      <div className="App">
+        <div className='goku'>
+          <img src={source()} width={500} />
+        </div>
+        <div className='main'>
+          <h1>SuG<span><img src={logo} height={50} style={{
+            position: 'relative',
+            top: 14
+          }} /></span>KU</h1>
+          <div className='row'>
+            <div className="board">
+              {board?.map((d, i) =>
+                d?.map((e, j) =>
+                  <input
+                    className={"board-item " + ((i === 0) ? "border-top " : "") + ((j === 0) ? "border-left " : "") + (((i + 1) % 3 === 0) ? "border-bottom-thick" : "")}
+                    value={e}
+                    onChange={({ target }) => {
+                      let copyBoard = board.map(d => d);
+                      copyBoard[i][j] = parseInt(target.value)
+                      setBoard(copyBoard)
+                    }}
+                  />)
+              )}
+            </div>
+          </div>
+          <div className='row'>
+            <button onClick={() => generateByDifficulty('easy')} className='easyButton'>Easy</button>
+            <button onClick={() => generateByDifficulty('medium')} className='mediumButton'>Medium</button>
+            <button onClick={() => generateByDifficulty('hard')} className='hardButton'>Hard</button>
+            <button onClick={() => generateByDifficulty(null)} className='randomButton'>Random</button>
+            <button onClick={() => { setBoard(Array(9).fill(null).map((u, i) => Array(9).fill(0))); setGrade(''); setValidate('') }} className='clearButton'>Clear</button>
+          </div>
+          <div className='row inputRow'>
+            <button onClick={() => grade(board, setGrade)} className='gradeButton'>Grade</button><input value={gradeResult} />
+            <input value={validateResult} /><button onClick={() => validate(board, setValidate)}>Validate</button>
+          </div>
+          <div className='row'>
+            <button onClick={() => solve(board, setBoard)} className="solveButton">Solve</button>
           </div>
         </div>
-        <div className='row'>
-          <button onClick={() => generateByDifficulty('easy')} className='easyButton'>Easy</button>
-          <button onClick={() => generateByDifficulty('medium')} className='mediumButton'>Medium</button>
-          <button onClick={() => generateByDifficulty('hard')} className='hardButton'>Hard</button>
-          <button onClick={() => generateByDifficulty(null)} className='randomButton'>Random</button>
-          <button onClick={() => { setBoard(Array(9).fill(null).map((u, i) => Array(9).fill(0))); setGrade(''); setValidate('') }} className='clearButton'>Clear</button>
-        </div>
-        <div className='row inputRow'>
-          <button onClick={() => grade(board, setGrade)} className='gradeButton'>Grade</button><input value={gradeResult} />
-          <input value={validateResult} /><button onClick={() => validate(board, setValidate)}>Validate</button>
-        </div>
-        <div className='row'>
-          <button onClick={() => solve(board, setBoard)} className="solveButton">Solve</button>
-        </div>
       </div>
-    </div>
   );
 }
 
